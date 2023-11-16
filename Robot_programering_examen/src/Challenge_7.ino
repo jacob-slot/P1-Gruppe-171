e00c8e64f3966f56a58a0a91ff52bffdf10ff44b
Jacob Slot <147039535+jacob-slot@users.noreply.github.com>
2023-11-16 13:13:30 +0100

    Update Challenge_7.ino

--- Robot_programering_examen/src/Challenge_7.ino
+++ Robot_programering_examen/src/Challenge_7.ino
@@ -8,6 +8,7 @@ Zumo32U4Motors motors;
 Zumo32U4Encoders encoders;
 Zumo32U4IMU imu;
 Zumo32U4OLED lcd;
+Zumo32U4Buzzer buzzer;
 
 #define NUM_SENSORS 3
 int16_t lineSensorValues[NUM_SENSORS];
@@ -56,143 +57,147 @@ int error = 0;
 int16_t speedLeft = 110;
 int16_t speedRight = 110;
 
+// Beep sound
+void bip() {
+  buzzer.playNote(NOTE_A(4), 20, 15);
+  delay(30);
+}
+
 // Read linesensors
-void readLineSensors(){
+void readLineSensors() {
   lineSensors.read(lineSensorValues, QTR_EMITTERS_ON);
 }
 
-void readEncoders(){
+void readEncoders() {
   countsLeft = encoders.getCountsLeft();
   countsRight = encoders.getCountsRight();
 }
 
-void resetEncoders(){
+void resetEncoders() {
   encoders.getCountsAndResetLeft();
   encoders.getCountsAndResetRight();
 }
 
-float radToDeg(float i){
-  return i*180/PI;
+float radToDeg(float i) {
+  return i * 180 / PI;
 }
 
-int mapLeft(){
+int mapLeft() {
   return map(lineSensorValues[0], lineSensorCal[0], lineSensorCalT[0], 0, 1000);
 }
 
-int mapMiddel(){
+int mapMiddel() {
   return map(lineSensorValues[1], lineSensorCal[1], lineSensorCalT[1], 0, 1000);
 }
 
-int mapRight(){
+int mapRight() {
   return map(lineSensorValues[2], lineSensorCal[2], lineSensorCalT[2], 0, 1000);
 }
 
-void updateMotorsToDriveStraight(){
+void updateMotorsToDriveStraight() {
 
-    if (error < 0.0)  // if error < 0 zumo to far to the right
-    {
-        motors.setSpeeds(speedLeft, speedRight + abs(error)*10); 
-    }
-    else              // if error > 0 zumo to far to the left
-    {
-        motors.setSpeeds(speedLeft + abs(error)*10, speedRight); 
-    }
+  if (error < 0.0)  // if error < 0 zumo to far to the right
+  {
+    motors.setSpeeds(speedLeft, speedRight + abs(error) * 10);
+  } else  // if error > 0 zumo to far to the left
+  {
+    motors.setSpeeds(speedLeft + abs(error) * 10, speedRight);
+  }
 }
 
-int calGyro(){
+int calGyro() {
 
-  for (uint16_t i = 0; i < 3000; i++)
-  {
+  for (uint16_t i = 0; i < 3000; i++) {
     // Wait for new data to be available, then read it.
-    while(!imu.gyroDataReady()) {}
+    while (!imu.gyroDataReady()) {}
     imu.readGyro();
 
     // Add the Z axis reading to the total.
     total += imu.g.z;
   }
-return total / 3000;
+  return total / 3000;
 }
 
-void turn(){
-  if (accumulated_rotation_deg > angleLine && angleLine < 0){
+void turn() {
+  if (accumulated_rotation_deg > angleLine && angleLine < 0) {
     motors.setSpeeds(-70, 70);
   }
 
-  if (accumulated_rotation_deg < angleLine && angleLine > 0){
+  if (accumulated_rotation_deg < angleLine && angleLine > 0) {
     motors.setSpeeds(70, -70);
   }
 
-  if (abs(accumulated_rotation_deg + angleLine) < 0.25){
+  if (abs(accumulated_rotation_deg + angleLine) < 0.25) {
     motors.setSpeeds(0, 0);
     stage = 5;
     t = 0;
   }
 }
 
-void allign(){
+void allign() {
 
-// Move forward
-motors.setSpeeds(70, 70);
+  // Move forward
+  motors.setSpeeds(70, 70);
 
-while (stage < 4){
+  while (stage < 4) {
 
-// Read sensors
-readLineSensors();
+    // Read sensors
+    readLineSensors();
 
-// Stage 1
-if (mapLeft() > value && stage == 1){
-  resetEncoders();
-  stage = 2;
-  side = 1;
-  Serial.println(stage);
+    // Stage 1
+    if (mapLeft() > value && stage == 1) {
+      resetEncoders();
+      stage = 2;
+      side = 1;
+      Serial.println(stage);
+    }
+
+    if (mapRight() > value && stage == 1) {
+      resetEncoders();
+      stage = 2;
+      side = 2;
+      Serial.println(stage);
+    }
+
+    // Stage 2
+    if (side == 1 && mapRight() > 450 && stage == 2) {
+      readEncoders();
+      motors.setSpeeds(0, 0);
+      stage = 3;
+      Serial.println(stage);
+    }
+
+    if (side == 2 && mapLeft() > 500 && stage == 2) {
+      readEncoders();
+      motors.setSpeeds(0, 0);
+      stage = 3;
+      Serial.println(stage);
+    }
+
+    // Stage 3
+    if (stage == 3 && side == 1) {
+
+      distLine = ((countsLeft + countsRight) / 2.0) / 910.0 * 11.466;
+      angleLine = radToDeg(atan(distLine / distSensor)) * (-1.0);
+      stage = 4;
+      lcd.clear();
+      lcd.print(angleLine);
+    }
+
+    if (stage == 3 && side == 2) {
+
+      distLine = ((countsLeft + countsRight) / 2.0) / 910.0 * 11.466;
+      angleLine = radToDeg(atan(distLine / distSensor)) * 1.07;
+      stage = 4;
+      lcd.clear();
+      lcd.print(angleLine);
+    }
+  }
 }
 
-if (mapRight() > value && stage == 1){
-  resetEncoders();
-  stage = 2;
-  side = 2;
-  Serial.println(stage);
-}
+void readGyro() {
 
-// Stage 2
-if (side == 1 && mapRight() > 450 && stage == 2){
-  readEncoders();
-  motors.setSpeeds(0, 0);
-  stage = 3;
-  Serial.println(stage);
-}
-
-if (side == 2 && mapLeft() > 500 && stage == 2){
-  readEncoders();
-  motors.setSpeeds(0, 0);
-  stage = 3;
-  Serial.println(stage);
-}
-
-// Stage 3
-if (stage == 3 && side == 1){
-
-  distLine = ((countsLeft + countsRight) / 2.0) / 910.0 * 11.466;
-  angleLine = radToDeg(atan(distLine / distSensor))*(-1.0);
-  stage = 4;
-  lcd.clear();
-  lcd.print(angleLine);
-}
-
-if (stage == 3 && side == 2){
-
-  distLine = ((countsLeft + countsRight) / 2.0) / 910.0 * 11.466;
-  angleLine = radToDeg(atan(distLine / distSensor)) * 1.07;
-  stage = 4;
-  lcd.clear();
-  lcd.print(angleLine);
-}
-}
-}
-
-void readGyro(){
-
-    if (imu.gyroDataReady() == true){
+  if (imu.gyroDataReady() == true) {
     // Read all IMU sensors: Gyro, Mag, Acc
     imu.read();
 
@@ -230,54 +235,54 @@ void setup() {
   delay(1000);
 
   // Print to Oled
-  lcd.print("Gyro");
-  Serial.println("Do not move Zumo, gyro calibration");
-  delay(1000);
+  lcd.print("Gyro Cal");
+  bip();
+  delay(500);
 
   // Cal gyro
   gyro_cal = calGyro() * (-1);
 
   // Calibrate line sensors for background
   lcd.clear();
-  lcd.print("Line");
+  lcd.print("Floor");
+  bip();
 
-  int32_t lineTotals[3] = {0, 0, 0};
+  int32_t lineTotals[3] = { 0, 0, 0 };
 
-    for (uint16_t i = 0; i < 500; i++){
-    
-      readLineSensors();
-      lineTotals[0] += lineSensorValues[0];
-      lineTotals[1] += lineSensorValues[1];
-      lineTotals[2] += lineSensorValues[2];
-      delay(5);
+  for (uint16_t i = 0; i < 500; i++) {
+
+    readLineSensors();
+    lineTotals[0] += lineSensorValues[0];
+    lineTotals[1] += lineSensorValues[1];
+    lineTotals[2] += lineSensorValues[2];
+    delay(5);
   }
 
   lineSensorCal[0] = lineTotals[0] / 500;
   lineSensorCal[1] = lineTotals[1] / 500;
   lineSensorCal[2] = lineTotals[2] / 500;
-  
+
   // move to tape
   lcd.clear();
-  lcd.print("Tape");
+  lcd.print("M to Tape");
 
-  Serial.println("Move to tape and press A");
   buttonA.waitForPress();
-  buttonA.waitForRelease();   // wait till the button is released
+  bip();
+  buttonA.waitForRelease();  // wait till the button is released
 
   delay(1000);
 
   // Calibrate line sensors for tape values
 
-  int32_t lineTotalsTape[3] = {0, 0, 0};
+  int32_t lineTotalsTape[3] = { 0, 0, 0 };
 
-    for (uint16_t i = 0; i < 500; i++){
-    
-      readLineSensors();
-      lineTotalsTape[0] += lineSensorValues[0];
-      lineTotalsTape[1] += lineSensorValues[1];
-      lineTotalsTape[2] += lineSensorValues[2];
-      delay(5);
+  for (uint16_t i = 0; i < 500; i++) {
 
+    readLineSensors();
+    lineTotalsTape[0] += lineSensorValues[0];
+    lineTotalsTape[1] += lineSensorValues[1];
+    lineTotalsTape[2] += lineSensorValues[2];
+    delay(5);
   }
 
   lineSensorCalT[0] = lineTotalsTape[0] / 500;
@@ -287,12 +292,14 @@ void setup() {
   // Done
 
   lcd.clear();
+  lcd.gotoXY(0, 0);
   lcd.print("Done");
+  lcd.gotoXY(0, 1);
+  lcd.print("Press A");
 
-  Serial.println("Done press A");
   buttonA.waitForPress();
-  buttonA.waitForRelease();   // wait till the button is released
-
+  bip();
+  buttonA.waitForRelease();  // wait till the button is released
 }
 
 void loop() {
@@ -301,23 +308,31 @@ void loop() {
   readLineSensors();
 
   // Get angle to line
-  if (stage < 4){
+  if (stage < 4) {
     allign();
   }
 
   // Turn parralel to line
-  if (stage == 4){
-    if (t == 0){start = millis(); t=1; accumulated_rotation_deg = 0;}
+  if (stage == 4) {
+    if (t == 0) {
+      start = millis();
+      t = 1;
+      accumulated_rotation_deg = 0;
+    }
     readGyro();
     turn();
   }
 
   // drive streight
-  if (stage == 5){
+  if (stage == 5) {
 
     // Set angle to 0
-    if (t == 0){start = millis(); t=1; accumulated_rotation_deg = 0;}
-    
+    if (t == 0) {
+      start = millis();
+      t = 1;
+      accumulated_rotation_deg = 0;
+    }
+
     // Read the gyro
     readGyro();
 
@@ -327,19 +342,17 @@ void loop() {
     updateMotorsToDriveStraight();
   }
 
-  if (stage == 5 && 
-      millis() - start > 2000 &&
-      mapMiddel() > 500){
-          // Stop program
-          motors.setSpeeds(0, 0);
-          stage = 10;
+  if (stage == 5 && millis() - start > 500 && (mapMiddel() > 500 || mapLeft() > 500 || mapRight() > 500)) {
+    // Stop program
+    motors.setSpeeds(0, 0);
+    stage = 10;
   }
 
   lcd.gotoXY(0, 1);
   lcd.print(accumulated_rotation_deg);
   lcd.gotoXY(0, 0);
 
-  if (buttonA.isPressed() == true){
+  if (buttonA.isPressed() == true) {
     stage = 1;
     t = 0;
     buttonA.waitForRelease();
