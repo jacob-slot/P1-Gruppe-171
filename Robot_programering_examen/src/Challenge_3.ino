97fb5131995613dad8c3d1f1c65f25d51acd3822
Rbczz03 <148756283+Rbczz03@users.noreply.github.com>
2023-11-27 12:47:16 +0100

    Update Challenge_3.ino
    Full code

--- Robot_programering_examen/src/Challenge_3.ino
+++ Robot_programering_examen/src/Challenge_3.ino
@@ -3,32 +3,65 @@
 //Setting up liberays to use later in the code.
 Zumo32U4ProximitySensors proxSensors;
 Zumo32U4Motors motors;
+Zumo32U4ButtonA buttonA;
+Zumo32U4LineSensors lineSensors;
+Zumo32U4OLED display;
 
+int Stage = 0;
+#define NUM_SENSORS 6
+uint16_t lineSensorValues[NUM_SENSORS];
 
 void setup() {
-  proxSensors.initThreeSensors();
-  proxSensors.setBrightnessLevels(10, 50);
 }
 
 
 void loop() {
-    wallTurn();
 
+  if (buttonA.isPressed()) {
+    Stage = 2;
+  }
+  switch (Stage) {
+    case 1:
+      motors.setSpeeds(0, 0);
+      break;
+
+    case 2:
+      wallTurn();
+      break;
+  }
 }
-void wallTurn()
-{
-  proxSensors.read();
 
-    if(proxSensors.countsRightWithRightLeds() > 40 && proxSensors.countsLeftWithLeftLeds() > 40){
-      motors.setSpeeds(0,0);
+
+void wallTurn() {
+#define NUM_SENSORS 6
+  uint16_t lineSensorValues[NUM_SENSORS];
+  int16_t Levels[10] = { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+  lineSensors.initThreeSensors();
+  proxSensors.initThreeSensors();
+  proxSensors.setBrightnessLevels(Levels, 10);
+  lineSensors.read(lineSensorValues, QTR_EMITTERS_ON);
+  proxSensors.read();
+  display.clear();
+  if (lineSensorValues[1] > 900) {
+    Stage = 1;
+  } else if (proxSensors.countsLeftWithLeftLeds() > proxSensors.countsRightWithRightLeds()) {
+    if (proxSensors.countsLeftWithLeftLeds() || proxSensors.countsRightWithRightLeds() == 0) {
+      if (proxSensors.countsFrontWithLeftLeds() > proxSensors.countsFrontWithRightLeds()) {
+        motors.setSpeeds(400, 200);
       }
-    else if(proxSensors.countsRightWithRightLeds()>proxSensors.countsLeftWithLeftLeds()){
-      motors.setSpeeds(15,150);
+    } else {
+      motors.setSpeeds(400, 200);
+    }
+  } else if (proxSensors.countsRightWithRightLeds() > proxSensors.countsLeftWithLeftLeds() || proxSensors.countsFrontWithRightLeds() > proxSensors.countsFrontWithLeftLeds()) {
+    if (proxSensors.countsRightWithRightLeds() || proxSensors.countsLeftWithLeftLeds() == 0) {
+      if (proxSensors.countsFrontWithRightLeds() > proxSensors.countsFrontWithLeftLeds()) {
+        motors.setSpeeds(200, 400);
       }
-    else if(proxSensors.countsLeftWithLeftLeds()>proxSensors.countsRightWithRightLeds()){
-      motors.setSpeeds(160,0);
-      }
-    else{
-      motors.setSpeeds(160,150);
-      }
+    } else {
+      motors.setSpeeds(200, 400);
+    }
+  } else {
+
+    motors.setSpeeds(400, 400);
+  }
 }
